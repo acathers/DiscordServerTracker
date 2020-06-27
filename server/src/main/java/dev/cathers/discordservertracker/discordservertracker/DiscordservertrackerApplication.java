5103eaab4fb40a6c5a75a13f9d7bf5c966647815
Anthony Cathers <anthonycathers@eworldes.com>
2020-06-26 17:48:37 -1000

    initial commit for spring-boot

---
+++ server/src/main/java/dev/cathers/discordservertracker/discordservertracker/DiscordservertrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.cathers.discordservertracker.discordservertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordservertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordservertrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/cathers/discordservertracker/discordservertracker/DiscordservertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cathers.discordservertracker.discordservertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordservertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
